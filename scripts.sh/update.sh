8c55b09c608670ad46e084124039b3ef3881fe36
totakaro <30659361+totakaro@users.noreply.github.com>
2021-05-14 04:43:55 -0500

    Update update.sh

--- scripts.sh/update.sh
+++ scripts.sh/update.sh
@@ -6,18 +6,18 @@
 
 # Update NBminer
 cd /tmp
-wget https://github.com/NebuTech/NBMiner/releases/download/v37.2/NBMiner_37.2_Linux.tgz
-tar -xzvf NBMiner_37.2_Linux.tgz
+wget https://github.com/NebuTech/NBMiner/releases/download/v37.3/NBMiner_37.3_Linux.tgz
+tar -xzvf NBMiner_37.3_Linux.tgz
 yes | cp -f /tmp/NBMiner_Linux/nbminer /opt/nbminer/nbminer
 
 # Update Gminer
 cd /tmp
-wget https://github.com/develsoftware/GMinerRelease/releases/download/2.53/gminer_2_53_linux64.tar.xz
-tar -xvf gminer_2_53_linux64.tar.xz
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz
+tar -xvf gminer_2_54_linux64.tar.xz
 yes | cp -f miner /opt/gminer/gminer
 
 # Update Lolminer
 cd /tmp
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.26/lolMiner_v1.26_Lin64.tar.gz
-tar -xvf lolMiner_v1.26_Lin64.tar.gz
-yes | cp -f 1.26/lolMiner /opt/lolminer/lolminer
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28a/lolMiner_v1.28a_Lin64.tar.gz
+tar -xvf lolMiner_v1.28a_Lin64.tar.gz
+yes | cp -f 1.28a/lolMiner /opt/lolminer/lolminer
