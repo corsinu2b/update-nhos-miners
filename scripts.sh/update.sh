0eb165f62024e2b9cd920512858515990546cde2
totakaro <30659361+totakaro@users.noreply.github.com>
2021-06-14 04:39:04 -0500

    Update Gminer to v2.56

--- scripts.sh/update.sh
+++ scripts.sh/update.sh
@@ -12,8 +12,8 @@ yes | cp -f /tmp/NBMiner_Linux/nbminer /opt/nbminer/nbminer
 
 # Update Gminer
 cd /tmp
-wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz
-tar -xvf gminer_2_54_linux64.tar.xz
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.56/gminer_2_56_linux64.tar.xz
+tar -xvf gminer_2_56_linux64.tar.xz
 yes | cp -f miner /opt/gminer/gminer
 
 # Update Lolminer
